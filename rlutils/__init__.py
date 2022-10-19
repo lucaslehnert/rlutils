ab8ceb2538b6ff8c347de27ea60740e1ae312d94
Lucas Lehnert <lucas.lehnert@mila.quebec>
2022-10-19 14:46:21 -0400

    updating example notebooks

--- rlutils/__init__.py
+++ rlutils/__init__.py
@@ -6,7 +6,7 @@
 
 from . import agent, algorithm, basisfunction, environment, logging, policy, data, plot, schedule
 from .utils import set_seeds, one_hot, Experiment, ExperimentException, repeat_function_with_ndarray_return
-from .types import TransitionSpec, Column, Action, Reward, Term, action_index_column, reward_column, term_column, Policy, Agent, TransitionListener
+from .types import TransitionSpec, Column, Action, Reward, Term, action_index_column, reward_column, term_column, Policy, Agent, TransitionListener, transition_listener
 
 __all__ = [
     "agent",
@@ -33,6 +33,7 @@ __all__ = [
     "term_column",
     "Policy",
     "Agent",
-    "TransitionListener"
+    "TransitionListener",
+    "transition_listener"
 ]
 __version__ = '0.0.2'
