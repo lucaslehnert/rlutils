db070bbdd89416d4c6a2131046a40b0aed3b3454
Lucas Lehnert <lucas.lehnert@mila.quebec>
2022-10-20 08:28:43 -0400

    updating task base class

--- rlutils/environment/Task.py
+++ rlutils/environment/Task.py
@@ -14,39 +14,15 @@ class Task(gym.Env):
             TransitionConfig: Transition specification for a task. 
         """
         pass
-
+    
+    @abstractmethod
     def step(
         self,
         action: Any
     ) -> Tuple[Dict[str, Any], Any, bool, bool, dict]:
         raise NotImplementedError
 
+    @abstractmethod
     def reset(self, *args: Any, **kvargs: Any) -> Tuple[Dict[str, Any], dict]:
         raise NotImplementedError
 
-    # @abstractmethod
-    # def state_defaults(self) -> Dict[str, np.ndarray]:
-    #     """Returns a dictionary of default state key-value paris. This
-    #     dictionary resembles an examplary state dictionary used to initialize
-    #     replay buffers. The values in this dictionary can be set to zero or
-    #     another custom default value.
-
-    #     :return: Default state dictionary.
-    #     :rtype: Dict[str, np.ndarray]
-    #     """
-    #     pass
-
-    # @abstractmethod
-    # def transition_defaults(self) -> Dict[str, np.ndarray]:
-    #     """Returns a dictionary of default transition key-value paris. This
-    #     dictionary resembles an examplary transition dictionary used to
-    #     initialize replay buffers. The values in this dictionary can be set to
-    #     zero or another custom default value.
-
-    #     This dictionary must contain the key `rlutils.data.ACTION`,
-    #     `rlutils.data.REWARD`, and `rlutils.data.TERM`.
-
-    #     :return: Default transition dictionary.
-    #     :rtype: Dict[str, np.ndarray]
-    #     """
-    #     pass
